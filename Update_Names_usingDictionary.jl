2b8834937b3b0e1377fa2d668c0002c1cba72cd5
guillemylla <guillemyllabou@gmail.com>
2020-06-24 16:00:46 -0400

    add fasta option

--- Update_Names_usingDictionary.jl
+++ Update_Names_usingDictionary.jl
@@ -5,10 +5,18 @@
 
 ## Useful to rename large genome fasta files, gff3 files, etc.
 
+## To rename fasta files use option --isFasta yes will make it much faster, since will only check headers
 
+##Usage:
+## Fasta
+#julia Update_Scaffold_Names_Juliaversion.jl --filetorename input.fa --dictionary Dictionary.txt --out out.fa --isFasta yes
+## nay text file
+#julia Update_Scaffold_Names_Juliaversion.jl --filetorename input.txt --dictionary Dictionary.txt --out output.txt --isFasta no
 
-
+# Guillem Ylla
 using ArgParse
+using FastaIO
+
 
 ######## Fucntion read input parameters
 function parse_commandline()
@@ -20,6 +28,9 @@ function parse_commandline()
         "--dictionary"
 			   help = "Dictionary file"
 			   required = true
+	 	"--isFasta"
+			   help = "yes if it's a fasta file. Will make it faster."
+			   required = false
 	     "--out","-o"
 	          help = "Output file"
 	          required = true
@@ -44,10 +55,17 @@ function create_julia_dict(inputdictfile)
 	end
 end
 
-####### Rename
 
-function make_names_dict(filein, fileout)
-	#i=1
+
+#######
+function replacename(contig)
+	  newline0=replace(newline, contig*r"\b" => Names_dictionary[contig])
+	  return(newline0)
+end
+
+####### Read and write
+function main_replace(filein, fileout)
+	i=1
 	open(fileout, "w") do fileout
 		open(filein) do file
 		    for line in eachline(file)
@@ -56,20 +74,54 @@ function make_names_dict(filein, fileout)
 					println("Found :: ",line)
 					for contig in keys(Names_dictionary)
 					      #println("Check contig ... ", contig )
-					      global newline=replace(newline, contig*r"\b" => Names_dictionary[contig])
+						  if occursin( contig*r"\b", newline)
+						  	newline=replacename(contig)
+							break# if replaced, stop trying. To make it faster
+						  end
 					end
 					println("New Name :: ",newline)
+					print("\n")
 				end
 				write(fileout, newline*"\n")
-				#if i ==10
-				#	 break
-				 #end
+				if i ==10000
+					 GC.gc()
+					 i=1
+				 end
 				#i=i	+1
 		    end
 		end
 	end
 	close(file)
 	close(fileout)
+end
+
+
+################# replace in fasta files
+function main_inFasta(filein, fileout)
+	#open(fileout, "w") do fileout
+	#fastawrite=open(FASTA.Writer, fileout)
+	#open(fileout, "w") do fileout
+	FastaWriter(fileout) do fastaout
+		FastaReader(filein) do rec
+			for (desc, seq) in rec
+		#fastareader = open(FASTA.Reader, filein)
+			#for rec in fastareader
+				global newline=desc
+				println("Input Sequence :: ", newline)
+					for contig in keys(Names_dictionary)
+						  if occursin( contig*r"\b", newline)
+							newline=replacename(contig)
+							break# if replaced, stop trying. To make it faster
+						  end
+					end
+
+				println("New seq name    :: " , newline)
+				writeentry(fastaout, newline, seq)
+			end
+			close(rec)
+		end
+		close(fastaout)
+	end
 end
 
 
@@ -81,7 +133,16 @@ function main()
         println("  $arg  =>  $val")
     end
 	create_julia_dict(parsed_args["dictionary"])
-	make_names_dict(parsed_args["filetorename"], parsed_args["out"])
+	if !isnothing(parsed_args["isFasta"])
+		if lowercase(parsed_args["isFasta"]) == "yes"
+			println("FASTA file")
+			main_inFasta(parsed_args["filetorename"], parsed_args["out"])
+		else
+			main_replace(parsed_args["filetorename"], parsed_args["out"])
+		end
+	else
+		main_replace(parsed_args["filetorename"], parsed_args["out"])
+	end
 	println("Done!!!")
 end
 
